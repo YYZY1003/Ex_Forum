093038a6d552579a2eff87381859254c3a16b7e0
YYZY1003 <105642994+YYZY1003@users.noreply.github.com>
2022-11-19 03:59:44 +0800

    2022 1119 03点59分

---
+++ src/main/java/com/yy/community/controller/AuthorizeController.java
@@ -0,0 +1,72 @@
+package com.yy.community.controller;
+
+import com.yy.community.dto.AccessTokenDTO;
+import com.yy.community.provider.GithubProvider;
+import com.yy.community.provider.dto.GithubUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+/**
+ * Created by codedrinker on 2019/4/24.
+ */
+@Controller
+@Slf4j
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback/{type}")
+    public String newCallback(
+            @RequestParam(name = "code") String code,
+            @RequestParam(name = "state", required = false) String state,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+
+        if (user != null) {
+            //登录成功
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            return "redirect:index";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response) {
+        request.getSession().invalidate();
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return "redirect:/";
+    }
+}
